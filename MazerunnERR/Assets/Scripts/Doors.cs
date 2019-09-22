c60503e89839017d57754c1859b29c9bca2d621a
ThEUNKnoWN01 <46256117+ThEUNKnoWN01@users.noreply.github.com>
2019-09-22 18:51:26 +0530

    WALL

--- MazerunnERR/Assets/Scripts/Doors.cs
+++ MazerunnERR/Assets/Scripts/Doors.cs
@@ -4,32 +4,46 @@ public class Doors : MonoBehaviour
 {
     Animator animator;
     bool doorOpen;
+    
 
     void Start()
     {
+
         doorOpen = false;
         animator = GetComponent<Animator>();
+
+
     }
 
     void OnTriggerEnter(Collider col)
     {
-        if(col.gameObject.tag == "Player")
+
+        if (col.gameObject.tag == "Player")
         {
+
             doorOpen = true;
             DoorControl("Open");
-        }
-    }
-    void OnTriggerExit(Collider col)
-    {
-        if (doorOpen)
-        {
-            doorOpen = false;
-            DoorControl("CLose");
+
         }
     }
 
-    void DoorControl( string direction)
+
+    void OnTriggerExit(Collider col)
     {
+
+        if (doorOpen)
+        {
+            doorOpen = false;
+            DoorControl("Close");
+
+        }
+    }
+
+
+    void DoorControl(string direction)
+    {
+
         animator.SetTrigger(direction);
+
     }
 }
